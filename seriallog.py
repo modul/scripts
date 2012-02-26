d91504a55bde4866f504c42e283e67706628adca
Remo Giermann <mo@liberejo.de>
2012-02-27 00:02:35 +0100

    seriallog: log to file too, as an option

--- seriallog.py
+++ seriallog.py
@@ -2,14 +2,14 @@
 #
 # Very simple serial logger
 #
-# Continuously reads and prints serial data.
+# Continuously reads and prints serial data
+# and optionally writes it to a logfile.
 #
-# seriallog.py DEVICE [BAUDRATE] [TIMEOUT]
+# seriallog.py DEVICE [BAUDRATE] [LOGFILE]
 #
 
 import sys
 import serial
-import readline
 
 argv = sys.argv
 argc = len(argv)
@@ -17,15 +17,16 @@ argc = len(argv)
 baudrate = 115200
 timeout = 1
 device = ''
+logfile = ''
 
 if argc > 1:
 	device = argv[1]
 	if argc > 2:
 		baudrate = int(argv[2])
 		if argc > 3:
-			timeout = int(argv[3])
+			logfile = argv[3]
 else:
-	print "Usage: %s DEVICE [BAUDRATE] [TIMEOUT]" % argv[0]
+	print "Usage: %s DEVICE [BAUDRATE] [LOGFILE]" % argv[0]
 	sys.exit(1)
 
 try:
@@ -38,10 +39,20 @@ print "opened %s with %i Bps, %.1fs timeout" % (device, baudrate, timeout)
 port.setWriteTimeout(10 * timeout)
 
 try:
+	log = open(logfile, 'w')
+except IOError as msg:
+	print msg
+	log = None
+else:
+	print "opened %s for writing" % logfile
+
+try:
 	while 1:
 		incoming = port.readlines()
 		if incoming:
 			for line in incoming:
 				print line,
+				if log:
+					print >> log, line,
 except KeyboardInterrupt:
 	sys.exit(0)
