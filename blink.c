d5f8996c69a26b730c8bc4d0cd51afcfdef02761
Remo Giermann <mo@liberejo.de>
2012-04-06 15:01:33 +0200

    blink.c: silly ThinkLight blinker

---
+++ blink.c
@@ -0,0 +1,54 @@
+/*
+ * blink.c
+ *
+ * This might let your ThinkLight blink!
+ *
+ * Usage: blink [COUNT] [DUTY/%] [PERIOD/ms]
+ *
+ * author: mo
+ * created: 2012/04/08
+ *
+ */
+
+#include <stdio.h>
+
+#define THINKLIGHT "/sys/class/leds/tpacpi::thinklight/brightness"
+#define OFF "0"
+#define ON  "255"
+
+#define COUNT 1
+#define PERIOD 500
+#define DUTY 25
+
+#define light(x, fp) fputs(x, fp); fflush(fp)
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int i;
+	int cnt = COUNT;
+	int per = PERIOD;
+	int dty = DUTY;
+
+	if (!(fp = fopen(THINKLIGHT, "w"))) { perror(THINKLIGHT); return 1; }
+	light(OFF, fp);
+
+	if (argc > 1 && sscanf(argv[1], "%u", &i) == 1) 
+		cnt = i;
+	if (argc > 2 && sscanf(argv[2], "%u", &i) == 1)
+		dty = i;
+	if (argc > 3 && sscanf(argv[3], "%u", &i) == 1)
+		per = i;
+
+	for (i=2*cnt; i>0; i--) {
+		if (i%2 == 0) {
+			light(ON, fp);
+			usleep(10 * per*dty);
+		}
+		else {
+			light(OFF, fp);
+			usleep(10 * per*(100-dty));
+		}
+	}
+	return 0;
+}
