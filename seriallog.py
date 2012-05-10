22dd83bceed2b86cbe025fc991db615ce4736797
Remo Giermann <mo@liberejo.de>
2012-05-10 18:12:30 +0200

    seriallog: option to send commands, wait interval

--- seriallog.py
+++ seriallog.py
@@ -10,30 +10,42 @@
 
 import sys
 import serial
+from time import sleep
 from argparse import FileType, ArgumentParser
+
+TIMEOUT = 0.5
+EOL = '\n'
 
 parser = ArgumentParser(description="Continously read serial data", usage="%(prog)s [options] device")
 parser.add_argument("device", help="serial device to open")
 parser.add_argument("--baudrate", "-b", default=115200, type=int, help="baudrate (bps) to set")
 parser.add_argument("--logfile", "-l", metavar="FILE", type=FileType(mode="w"), help="file to log to")
+parser.add_argument("--send", metavar="CMD", nargs="+", help="write serial commands, then read")
+parser.add_argument("--wait", metavar="t", default=0, type=float, help="wait before reading")
 parser.add_argument("--quiet", "-q", action="store_true", help="don't print to stdout")
-parser.add_argument("--timeout", default=0.5, type=float, help="timeout for serial read/write")
 
 args = parser.parse_args()
 
 try:
-	port = serial.Serial(args.device, args.baudrate, timeout=args.timeout)
+	port = serial.Serial(args.device, args.baudrate, timeout=TIMEOUT)
 except serial.serialutil.SerialException as msg:
 	print msg
 	sys.exit(1)
 else:
-	print "opened %s with %i Bps, %.1fs timeout" % (args.device, args.baudrate, args.timeout)
+	print "opened %s with %i Bps, %.1fs interval" % (args.device, args.baudrate, args.wait)
 
 if args.logfile:
 	print "opened %s for writing" % (args.logfile.name)
 
 try:
 	while 1:
+		if args.send:
+			for cmd in args.send:
+				port.write(cmd+EOL)
+
+		if args.wait:
+			sleep(args.wait)
+
 		incoming = port.readlines()
 		if incoming:
 			for line in incoming:
