7a7fe343d438aed8eb7992d5d6eb1219a9ee2291
Remo Giermann <mo@liberejo.de>
2012-05-22 20:17:12 +0200

    seriallog: line wrapping implemented
    -> v0.3.2
    via --width lines can be wrapped to N characters per line
    or, with hex, bin or decimal dumps, to N bytes per line.

--- seriallog.py
+++ seriallog.py
@@ -23,7 +23,7 @@ import serial
 import time
 from argparse import FileType, ArgumentParser
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 hexs = lambda text: ' '.join("{:02x}".format(x) for x in bytearray(text))
 bins = lambda text: ' '.join("{:08b}".format(x) for x in bytearray(text))
@@ -47,23 +47,41 @@ def mk_seconds(start):
 		return "{:6.2f}".format(time.time()-start)
 	return seconds
 
-def formatter(flist=[], conv=None, sep="| "):
+def formatter(flist=[], conv=None, width=0, sep="| "):
 	''' Build a function that prepends some info strings to a string,
 	which is, if conv is one of hexs, decs or bins, first converted
 	to a hex, decimal or binary dump.
 	Strings to be prepended are retrieved by calling functions in flist and
 	are separated from the base string using 'sep'.
+	When width > 0, text is wrapped to 'width' characters per line or
+	'width' number of bytes per line if conv is used.
 	'''
-	if not callable(conv):
-		convert = lambda text: text
-	else:
-		convert = lambda text: conv(text)
+	from textwrap import fill
 
 	if flist:
 		flist += [sep]
 
+	if not callable(conv):
+		chars = 1
+		convert = lambda text: text
+	else:
+		chars = len(conv('\xff')) + 1
+		convert = lambda text: conv(text)
+	
+	if width:
+		def par(indent, text):
+			return fill(text, width*chars + indent,
+					subsequent_indent=' '*indent,
+					replace_whitespace=False,
+					drop_whitespace=False)
+	else:
+		def par(indent, text):
+			return text
+
 	def _formatter(line):
-		return ' '.join(callable(f) and f() or str(f) for f in flist) + convert(line)
+		prefix = ' '.join(callable(f) and f() or str(f) for f in flist)
+		return par(len(prefix), prefix + convert(line))
+
 	return _formatter
 
 
@@ -83,6 +101,7 @@ group.add_argument("--timestamp", action="append_const", dest="timestamps", cons
 group.add_argument("--seconds", action="append_const", dest="timestamps", const=mk_seconds(time.time()), help="prepend seconds since start", default=[])
 
 group = parser.add_argument_group("Format")
+group.add_argument("--width", default=70, type=int, help="number of characters/bytes per line")
 group.add_argument("--hex", dest="converter", action="store_const", const=hexs, default=None, help="display responses as hex")
 group.add_argument("--binary", dest="converter", action="store_const", const=bins, default=None, help="display responses as decimal")
 group.add_argument("--decimal", dest="converter", action="store_const", const=decs, default=None, help="display responses as binary")
@@ -104,7 +123,7 @@ else:
 	if args.logfile:
 		print "opened %s for logging" % (args.logfile.name)
 
-fmt = formatter(args.timestamps, args.converter)
+fmt = formatter(args.timestamps, args.converter, args.width)
 
 try:
 	while 1:
