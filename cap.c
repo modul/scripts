47d708afb523afb3eaf67df4db80a4b6d6528f10
Remo Giermann <mo@liberejo.de>
2012-08-21 00:13:25 +0200

    cap: Cellular Automata PBM generator

---
+++ cap.c
@@ -0,0 +1,74 @@
+/* 
+ * Print Cellular Automata output in netpbm format.
+ * 
+ * All rights reversed.
+ * 2012 Remo Giermann <mo@liberejo.d>
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define W 1000
+
+void usage(const char *p)
+{
+	printf("Usage: %s [-h|--help] [b|w] [rRULE] [wWIDTH] [OUTFILE]\n", p);
+	exit(0);
+}
+
+void pbm(int *u, size_t w, size_t h, FILE *o, int b, const char *desc)
+{
+	int x, y;
+	fprintf(o, "P1\n# %s\n%u %u\n", desc, w, h);
+	for (y=0; y<h; y++) {
+		for (x=0; x<w; x++)
+			fprintf(o, "%u ", u[y*w+x]?b:!b);
+		fputc(10, o);
+	}
+}
+
+int ca(int *u, size_t w, size_t max, int rule)
+{
+	int x, t;
+	for (t=0; t<max-1; t++) {
+		for (x=0; x<w; x++) {
+			int l = x==0? 0: u[t*w+x-1];
+			int c = u[t*w+x];
+			int r = x==w-1? 0: u[t*w+x+1];
+			int n = (l<<2)|(c<<1)|(r<<0);
+			u[(t+1)*w+x] = (rule&(1<<n))>>n;
+		}
+		if (u[t*w] && u[t*w+w-1]) break;
+	}
+	return t+1;
+}
+
+int main(int argc, const char **argv)
+{
+	int t, b, *u, w=W, r=30, h=0;
+	FILE *out = stdout;
+	char c[32];
+	while (--argc > 0) {
+		if (strcmp(argv[argc], "-h") == 0 || strcmp(argv[argc], "--help") == 0) 
+			usage(argv[0]);
+		else if (sscanf(argv[argc], "%c%u", c, &t) == 2) {
+			if (*c == 'r') r = t;
+			else if (*c == 'w') w = t;
+			else if (!(out = fopen(argv[argc], "w"))) {
+				perror(argv[argc]); usage(argv[0]);
+			}
+		}
+		else if (sscanf(argv[argc], "%c", c) == 1)
+			b = *c=='b'? 0: 1;
+	}
+
+	h = w*3/2;
+	u = malloc(w*h*sizeof(int));
+	u[w/2] = 1;
+
+	sprintf(c, "CA rule %u", r);
+	t = ca(u, w, h, r);
+	pbm(u, w, t, out, b, c);
+
+	return 0;
+}
